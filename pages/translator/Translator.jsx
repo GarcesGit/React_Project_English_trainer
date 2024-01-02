b5d451868a52319da2ec3f07426801d513326e72
Natalya Koshukova <koshukovanat@gmail.com>
2024-01-02 22:49:52 +0700

    ..

--- pages/translator/Translator.jsx
+++ pages/translator/Translator.jsx
@@ -17,12 +17,11 @@ import ball_gb_img from '../../images/ball_gb.png';
 function Translator({ allWords }) {
 
 	const [startWord, setStartWord] = useState({});
-	const [translationWord, setTranslationWord] = useState('');
-	const ref = useRef();
+	const ref = useRef(null);
+	const [message, setMessage] = useState('');
 
 	const getRandomWord = (arr) => {
 		const randomIndex = Math.floor(Math.random() * arr.length);
-		// setTranslationWord(arr[randomIndex].translation);
 		setStartWord(arr[randomIndex]);
 	}
 
@@ -30,52 +29,70 @@ function Translator({ allWords }) {
 		getRandomWord(allWords);
 	}, []);
 
-	// Стейт В инпете Value=tr.input, сделать в инпуте А onChange,  SettranslationInput
+	const checkTranslation = (event) => {
+		if (event.key === 'Enter') {
+				if (ref.current.value === startWord.translation) {
+					setMessage('Отлично! Следующее слово.');
+					getRandomWord(allWords);
+				} else {
+					setMessage('Попробуйте еще раз.');
+				}
+					ref.current.value = '';
+					
+					// removeEntry(startWord)
+				// console.log(startWord);
+				// console.log(allWords);
+		}
+	}
+
+// const removeEntry = (entry) => {
+// 	setStartWord(allWords.filter(entries => entries.id !== entry.id))
+// }
+
 //absolute: "абсолютный", accept: "принять", account: "счет", accountant: "бухгалтер", achieve: "достигнуть"
 
-	return (
-		<div className="wrapper">
-			<div className="container-fluid">
-				<div className="row">
-					<div className="title">
+return (
+	<div className="wrapper">
+		<div className="container-fluid">
+			<div className="row">
+				<div className="title">
 					<img src={title_img} alt="" className="title_img" />
-					</div>
-					<div className="a2 container_lang">
-						<img src={button_en_img} alt="" className="lang_img" />
-						<Button variant='primary rotate'>&#8596;</Button>
-						<img src={button_ru_img} alt="" className="lang_img" />
-					</div>
-					<div className="col-xs-6 col-sm-5 col-md-4 col-xl-3 a3 ">
-						<p name='initialWord' className="form-control initialWord">
-							{startWord.initialWord}
-						</p>
-					</div>
-					<div className="col-xs-6 col-sm-5 col-md-4 col-xl-3 a4">
+				</div>
+				<div className="a2 container_lang">
+					<img src={button_en_img} alt="" className="lang_img" />
+					<Button variant='primary rotate'>&#8596;</Button>
+					<img src={button_ru_img} alt="" className="lang_img" />
+				</div>
+				<div className="col-xs-6 col-sm-5 col-md-4 col-xl-3 a3 ">
+					<p name='initialWord' className="form-control initialWord">
+						{startWord.initialWord}
+					</p>
+				</div>
+				<div className="col-xs-6 col-sm-5 col-md-4 col-xl-3 a4">
 					<InputTranslation
-						type="text" 
-						name='translation' 
-						className="form-control translation" 
+						type="text"
+						name='translation'
+						className="form-control translation"
 						placeholder="Введите перевод и нажмите Enter"
 						ref={ref}
-						onChange={event => setTranslationWord(event.target.value)}
-
-						/>
-					</div>
-						<p className="error">text</p>
-					<div className="col-sm-5 col-md-4 col-xl-3 a6 container_balls">
-						<img src={ball_am3_img} alt="" className="ball ball_am3_img" />
-						<img src={ball_can2_img} alt="" className="ball ball_can2_img" />
-						<img src={ball_nz2_img} alt="" className="ball ball_nz2_img" />
-						<img src={ball_nz_img} alt="" className="ball ball_nz_img" />
-						<img src={ball_gb2_img} alt="" className="ball ball_gb2_img" />
-						<img src={ball_au_img} alt="" className="ball ball_au_img" />
-						<img src={ball_am_img} alt="" className="ball ball_am_img" />
-						<img src={ball_gb_img} alt="" className="ball ball_gb_img" />
-					</div>
+						onKeyDown={checkTranslation}
+					/>
+				</div>
+				<p className="error">{message}</p>
+				<div className="col-sm-5 col-md-4 col-xl-3 a6 container_balls">
+					<img src={ball_am3_img} alt="" className="ball ball_am3_img" />
+					<img src={ball_can2_img} alt="" className="ball ball_can2_img" />
+					<img src={ball_nz2_img} alt="" className="ball ball_nz2_img" />
+					<img src={ball_nz_img} alt="" className="ball ball_nz_img" />
+					<img src={ball_gb2_img} alt="" className="ball ball_gb2_img" />
+					<img src={ball_au_img} alt="" className="ball ball_au_img" />
+					<img src={ball_am_img} alt="" className="ball ball_am_img" />
+					<img src={ball_gb_img} alt="" className="ball ball_gb_img" />
 				</div>
 			</div>
 		</div>
-	);
+	</div>
+);
 }
 
 export default Translator;
